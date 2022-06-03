6095e9d0e0d625eebc43e44105ecae9ddc98cbec
Cyph3r T3xt <cyphercrime@gmail.com>
2022-06-03 07:37:53 +0530

    Fixed the glitch in cp command

--- master_multiple.sh
+++ master_multiple.sh
@@ -12,7 +12,7 @@ fi
 # Prevent users from viewing other users' processes
 mount -o remount,rw,hidepid=2 /proc
 
-cp ~/$linux_users_credentials_file ~/$linux_users_credentials_file'.bak'
+cp ~/$linux_users_credentials_file ~/$linux_users_credentials_file'.bak' 2>/dev/null || :
 rm -f ~/$linux_users_credentials_file
 
 for (( i=1; i<=$count; i++ ))
